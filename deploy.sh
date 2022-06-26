305ce80580a0d0eeedc467148daebf8333af189c
anvaka <anvaka@gmail.com>
2022-06-26 15:57:37 -0700

    use main as default branch

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ git init
 git add .
 git commit -m 'push to gh-pages'
 ## Change the line below to deploy to your gh-pages
-git push --force git@github.com:anvaka/graph-start.git master:gh-pages
+git push --force git@github.com:anvaka/graph-start.git main:gh-pages
 cd ../
